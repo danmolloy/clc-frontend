725cbf92e3d8d4b805229cb65ae3e6e6eebe0f87
Daniel Molloy <danielmolloy_6@icloud.com>
2021-07-30 11:33:40 +1000

    updated json

--- src/Artists.js
+++ src/Artists.js
@@ -2,28 +2,32 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Artists = () => {
-  const [artistName, setArtistName] = useState(null)
-  const [albums, setAlbums] = useState(null)
-  const [members, setMembers] = useState(null)
+  const [artistName, setArtistName] = useState([])
 
   useEffect(() => {
     getData()
-  })
+  }, [])
 
   const getData = () => {
     fetch('https://raw.githubusercontent.com/danmolloy/clc-frontend/main/public/data.json')
     .then(res => res.json())
     .then(myJson => {
-      setArtistName(myJson["artist"])
-      setAlbums(myJson['albums'])
-      setMembers(myJson['members'])
+      for (let i = 0; i < myJson.length; i++) {
+        setArtistName([...artistName, myJson[i]['artist']])
+      }
     })
   }
+
+  const formattedArtists = artistName.map(i => 
+    <Link to={`/artists/${artistName}`} key={artistName}>{artistName}</Link>
+    )
+
+
 
   return (
     <div className="main">
       <h2>Artists</h2>
-      <Link to={`/artists/${artistName}`}>{artistName}</Link>
+      {formattedArtists}
     </div>
   )
 }
