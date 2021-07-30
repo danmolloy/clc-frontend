d61c0cdce3e7c6fdfc0d465a6cb123d34c9adc70
Daniel Molloy <danielmolloy_6@icloud.com>
2021-07-30 16:13:52 +1000

    single artist page fetches data

--- src/components/SingleArtist.js
+++ src/components/SingleArtist.js
@@ -1,13 +1,45 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export const SingleArtist = () => {
+  const [albums, setAlbums] = useState(null)
+  const [members, setMembers] = useState(null)
   const { artistName } = useParams();
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = () => {
+    fetch('https://raw.githubusercontent.com/danmolloy/clc-frontend/main/public/data.json')
+    .then(res => res.json())
+    .then(myJson => {
+      for (let i = 0; i < myJson.length; i ++) {
+        if (myJson[i]["artist"] === artistName) {
+          setAlbums(myJson[i]["albums"])
+          setMembers(myJson[i]["members"])
+        }
+      }
+    })
+  }
+
+  const renderedAlbums = () => {
+    return albums.map(i => 
+      <p key={i[1]}>
+        {`${i[1]} (${i[0]})`}
+      </p>
+      )
+  }
+  
+
 
   return (
     <div className="main">
       <h2>{artistName}</h2>
       <h3>Albums:</h3>
+      <p>{albums}</p>
       <h3>Members:</h3>
+      <p>{members}</p>
     </div>
   )
 }
